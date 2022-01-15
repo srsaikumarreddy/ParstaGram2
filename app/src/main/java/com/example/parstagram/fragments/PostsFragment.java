2a1c114e19ae8efe6a00839525596f52e6ed4a88
Sai <srsaikumarreddy@gmail.com>
2022-01-15 00:00:59 -0800

    Swipe Refresh added

--- app/src/main/java/com/example/parstagram/fragments/PostsFragment.java
+++ app/src/main/java/com/example/parstagram/fragments/PostsFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -34,6 +35,7 @@ public class PostsFragment extends Fragment {
     public static final String TAG="PostsFragment";
     protected PostsAdapter adapter;
     protected List<Post>allPosts;
+    SwipeRefreshLayout swipeContainer;
 
     public PostsFragment() {
         // Required empty public constructor
@@ -50,6 +52,18 @@ public class PostsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        swipeContainer =view.findViewById(R.id.swipeContainer);
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i(TAG,"onRefresh");
+                queryPosts();
+            }
+        });
 
         rvPosts=view.findViewById(R.id.rvPosts);
 
